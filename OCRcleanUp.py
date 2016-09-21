04c801c48612343f98fa6718bc4e0ec7ffc2c5bd
Julia Duimovich <duimovich.julia@gmail.com>
2016-09-21 15:30:04 -0400

    added indexing code and stripped out Chapter headings in the cleanup script

--- OCRcleanUp.py
+++ OCRcleanUp.py
@@ -1,8 +1,8 @@
 '''Script to clean up the original OCR file
-looks easiest to rewrite out to new file. Line by line means only curr line in RAM.
 
 we want to:
 	-get rid of non letter characters. 
+	- get rid of "CHAPTER #"
 	-replace dashes with spaces unless EOL
 	-get rid of line denoting the next file
 	-make all characters lowercase
@@ -10,42 +10,42 @@ we want to:
 
 import re
 
-input= open('testOCRFull.txt', 'r')
-output=open('cleanTestData.txt','w')
-newFileRE=re.compile("-+File:")
-figRE=re.compile("^.? ?[Ff]ig")
-reunite=None
+with open('testOCRFull.txt', 'r') as inputFile:
 
-for line in input:
+	with open('cleanTestData.txt','w') as output:
 
-	if line.isspace():
-		continue
-	if newFileRE.match(line) or figRE.match(line):
-		continue
-		
-	line=line.lower()
-	
-	if reunite !=None:
-		reunite=re.sub("[^a-z]", "",reunite)
-		line=reunite+ line
+
+		newFileRE=re.compile("-+File:")
+		figRE=re.compile("^.? ?[Ff]ig")
 		reunite=None
-		
-		
-	if line.rstrip()[-1]=='-' and line.rstrip()[-2]!="-": #line ends with - if you ignore whitespace, but not multiple dashes.
-		#save the word start
-		reunite=line.rpartition(" ")[-1]
-		#remove it from original line
-		line=line.rsplit(' ', 1)[0]
 
-		
-	#now replace all with space
-	line=line.replace("-"," ")
-	#now strip all non letter chars.
-	line=re.sub("[^a-z \n]", "",line)
-	#write to new file
-	output.write(line)
-	
-input.close()
-output.close()
-		
+		for line in inputFile:
 
+			if line.isspace():
+				continue
+			if newFileRE.match(line) or figRE.match(line):
+				continue
+			if "CHAPTER" in line:
+				continue
+				
+			line=line.lower()
+			
+			if reunite !=None:
+				reunite=re.sub("[^a-z]", "",reunite)
+				line=reunite+ line
+				reunite=None
+				
+				
+			if line.rstrip()[-1]=='-' and line.rstrip()[-2]!="-": #line ends with - if you ignore whitespace, but not multiple dashes.
+				#save the word start
+				reunite=line.rpartition(" ")[-1]
+				#remove it from original line
+				line=line.rsplit(' ', 1)[0]
+
+				
+			#now replace all with space
+			line=line.replace("-"," ")
+			#now strip all non letter chars.
+			line=re.sub("[^a-z \n]", "",line)
+			#write to new file
+			output.write(line)
--- fuzzySearch.py
+++ fuzzySearch.py
@@ -1,5 +1,11 @@
 #the mock one we know works slowly (to get unit tests done)
+
+#A more efficient method would never repeat the same distance calculation.
+#For example, the Levenshtein distance of all possible prefixes might be stored in an array d[][]
+#where d[i][j] is the distance between the first i characters of string s and the first j characters of string t. The table is easy to construct one row at a time starting with row 0. When the entire table has been built, the desired distance is d[len_s][len_t].
 def lev(w1, w2):
+	#Calculates the distance between 2 words
+	#TODO: rewrite as a dynamic programming algorithm (curently shitty + recursive)
 	if not w1:
 		return len(w2)
 	if not w2:
@@ -8,6 +14,7 @@ def lev(w1, w2):
 
 	
 def leven(w1, w2, errMax):
+	#calculates if 2 words are within the error bound wrt distance.
 	#w2 smaller pls:
 	if len(w1) < len(w2):
 		return leven(w2, w1, errMax)
@@ -22,7 +29,7 @@ def leven(w1, w2, errMax):
 				errCount+=1
 	
 
-	#if the length difference is too big, we aint even bothering to calculate
+	#if the length difference is too big, we aren't even bothering to calculate
 	if len(w1)-len(w2)- errMax > 0:
 		return False
 	
@@ -41,24 +48,43 @@ def main():
 	pass
 	
 
-def index():
-	#create a list of lists: [[cat, 1, 6, 100], mat[2, 105, 238]] that acts as an index.
-	#will be alphabetical to help with searches.
-
-def readFile():
-	#makes the line by line list [ maybe better to index/read @ same time]
-	
-
 def search(w1, maxDiff):
 	#Returns a list of the words that match the upper bound. 
-	#
- 
- 
-def writeStats(word, maxDiff, numMatches):
-	#adds data point to end of file of word, errMax, number of levenshtein matches.
-	statsFile=open("searchRecord.txt", 'a') 
-	statsFile.write("{0},{1},{2}".format(word, maxDiff, numMatches)
-	statsFile.close()
+	pass
+
+
+def read_saved_index(fileName):
+	with open(fileName, 'r') as file:
+		index=json.load(file)
+	return index
+
+
+def dump_index(index, fileName):
+	with open(fileName,"w") as file:
+		json.dump(index, file)
 	return
-	
-	
+
+
+def index_read_file(fileName):
+	#create a word index: {"cat":[1, 6, 100], 'mat':[2, 105, 238]}
+	lineCount=1
+	index={}
+	with open(fileName, "r") as data:
+		for line in data:
+			words=line.split()
+			for x in words:
+				if x not in index:
+					index[x]=[linecount]
+				else:
+					index[x].append(lineCount)
+
+			lineCount+=1
+	return index
+					
+
+ 
+def write_stats(word, maxDiff, numMatches):
+	#adds data point to end of file of word, errMax, number of levenshtein matches.
+	with open("searchRecord.txt", "a") as statsFile:
+		statsFile.write("{0},{1},{2}".format(word, maxDiff, numMatches))
+	return
