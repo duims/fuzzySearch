9aaabe4bdca1e6a5ac0701b5f30c3f1680b5d4af
Julia Duimovich <duimovich.julia@gmail.com>
2016-09-21 18:53:05 -0400

    rewrote the actual lev function to use the matrix algorithm

--- fuzzySearch.py
+++ fuzzySearch.py
@@ -1,90 +1,108 @@
-#the mock one we know works slowly (to get unit tests done)
+import json, itertools
 
-#A more efficient method would never repeat the same distance calculation.
-#For example, the Levenshtein distance of all possible prefixes might be stored in an array d[][]
-#where d[i][j] is the distance between the first i characters of string s and the first j characters of string t. The table is easy to construct one row at a time starting with row 0. When the entire table has been built, the desired distance is d[len_s][len_t].
+
 def lev(w1, w2):
-	#Calculates the distance between 2 words
-	#TODO: rewrite as a dynamic programming algorithm (curently shitty + recursive)
-	if not w1:
-		return len(w2)
-	if not w2:
-		return len(w1)
-	return min(lev(w1[1:], w2[1:])+(w1[0] != w2[0]), lev(w1[1:], w2)+1, lev(w1, w2[1:])+1)
+    # Calculates the distance between 2 words
+    mat = [[] for i in range(len(w1))]
+    for i in range(len(w1)):
+        for j in range(len(w2)):
+            mat[i].append(0)
 
-	
+    if w1 == w2:
+        return 0
+
+        # Hamming
+    diff = 0
+
+    if len(w1) == len(w2):
+        for i in range(len(w1)):
+            if w1[i] != w2[i]:
+                diff += 1
+            return diff
+
+    if len(w1) == 0:
+        return len(w2)
+    if len(w2) == 0:
+        return len(w1)
+
+    # set up matrix boundaries
+    for i in range(len(w1)):
+        mat[i][0] = i
+    for j in range(len(w2)):
+        mat[0][j] = j
+
+    for j in range(len(w2)-1):
+        for i in range(len(w2)-1):
+            if w1[i - 1] == w2[j - 1]:
+                mat[i][j] = mat[i - 1][j - 1]
+            else:
+                mat[i][j] = min(mat[i - 1][j] + 1,
+                                mat[i][j - 1] + 1,
+                                mat[i - 1][j - 1] + 1)
+    return mat[i][j]
+
+
 def leven(w1, w2, errMax):
-	#calculates if 2 words are within the error bound wrt distance.
-	#w2 smaller pls:
-	if len(w1) < len(w2):
-		return leven(w2, w1, errMax)
-	errCount=0
-	if w1==w2:
-		return True
-		
-	#hamming
-	if len(w1)==len(w2):
-		for i in range(len(w1)):
-			if w1[i]!= w2[i]:
-				errCount+=1
-	
+    # calculates if 2 words are within the error bound wrt distance
+    # Calls lev(w1, w2)
+    #w2 smaller pls:
 
-	#if the length difference is too big, we aren't even bothering to calculate
-	if len(w1)-len(w2)- errMax > 0:
-		return False
-	
-	#let's cheat using the terrible recursive version for now so we can get test done
-	
-	errCount=lev(w1, w2)
-	
-	if errCount<=errMax:
-		return True
-	else:
-		return False
-		
-		
+    #if the length difference is too big for our search params, we aren't even bothering to calculate
+    if abs(len(w1) - len(w2))- errMax > 0:
+        return False
+
+    errCount = lev(w1, w2)
+
+    if errCount <= errMax:
+        return True
+    else:
+        return False
+
 
 def main():
-	pass
-	
+    # index=index_read_file("text/cleanTestData.txt")
+    # dump_index(index,"text/indexDump.txt")
+    #index = read_saved_index("text/indexDump.txt")
+ 
 
 def search(w1, maxDiff):
-	#Returns a list of the words that match the upper bound. 
-	pass
+    # Returns w1 list of the words that match the upper bound.
+    pass
 
 
 def read_saved_index(fileName):
-	with open(fileName, 'r') as file:
-		index=json.load(file)
-	return index
+    with open(fileName, 'r') as file:
+        index = json.load(file)
+    return index
 
 
 def dump_index(index, fileName):
-	with open(fileName,"w") as file:
-		json.dump(index, file)
-	return
+    with open(fileName, "w") as file:
+        json.dump(index, file)
+    return
 
 
 def index_read_file(fileName):
-	#create a word index: {"cat":[1, 6, 100], 'mat':[2, 105, 238]}
-	lineCount=1
-	index={}
-	with open(fileName, "r") as data:
-		for line in data:
-			words=line.split()
-			for x in words:
-				if x not in index:
-					index[x]=[linecount]
-				else:
-					index[x].append(lineCount)
+    # create w1 word index: {"cat":[1, 6, 100], 'mat':[2, 105, 238]}
+    lineCount = 1
+    index = {}
+    with open(fileName, "r") as data:
+        for line in data:
+            words = line.split()
+            for x in words:
+                if x not in index:
+                    index[x] = [lineCount]
+                else:
+                    index[x].append(lineCount)
 
-			lineCount+=1
-	return index
-					
+            lineCount += 1
+    return index
 
- 
+
 def write_stats(word, maxDiff, numMatches):
-	#adds data point to end of file of word, errMax, number of levenshtein matches.
-	with open("searchRecord.txt", "a") as statsFile:
-		statsFile.write("{0},{1},{2}".format(word, maxDiff, numMatches))
-	return
+    # adds data point to end of file of word, errMax, number of levenshtein matches.
+    with open("searchRecord.txt", "w1") as statsFile:
+        statsFile.write("{0},{1},{2}".format(word, maxDiff, numMatches))
+    return
+
+main()
