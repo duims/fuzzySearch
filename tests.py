945c997f6b3e7cb777e673076a4a1f5a9ab9bb19
Julia Duimovich <duimovich.julia@gmail.com>
2016-09-21 18:53:24 -0400

    added specific lev tests

--- tests.py
+++ tests.py
@@ -6,11 +6,19 @@ class TestFuzzySearch(unittest.TestCase):
 
 
 	def test_leven(self):
-		self.assertTrue(fuzzySearch.leven("cat", "cat", 0))
 		self.assertFalse(fuzzySearch.leven("c", "abcdefghi",1 ))
+		self.assertTrue(fuzzySearch.leven("cat", "cat", 0))
 		self.assertTrue(fuzzySearch.leven("cat", "cran", 2))
-		self.assertFalse(fuzzySearch.leven("kitten", "siting",2))
-		
+		self.assertFalse(fuzzySearch.leven("kitten", "sitting",2))
+	
+	def test_lev(self):
+		self.assertNotEqual(fuzzySearch.lev("abcdefghi","c"), 1)
+		self.assertEqual(fuzzySearch.lev("cat", "cat"), 0)
+		self.assertEqual(fuzzySearch.lev("cat", "cran"), 2)
+		self.assertNotEqual(fuzzySearch.lev("kitten", "sitting"),2)
+
+
+
 if __name__=='__main__':
         unittest.main()
 
